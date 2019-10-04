9ca1a5eb1b02a9911a42a3141d71a60d26722d77
CarlosRobertoMedeiros <carlosmedeiroslima@gmail.com>
2019-10-04 06:29:44 -0300

    Finalização do Desenvolvimento dos Serviços

--- modulos/cursoModulo16/implementando-comunicacao-back-front/src/app/app.component.ts
+++ modulos/cursoModulo16/implementando-comunicacao-back-front/src/app/app.component.ts
@@ -15,24 +15,43 @@ export class AppComponent implements OnInit {
   constructor(private cidadeService: CidadeService) {};
 
   ngOnInit(){
-    this.cidadeService.consultar()
+    this.consultar();
+  }
+
+  private consultar(){
+      this.cidadeService.consultar()
       .then(cidades => {
         this.cidades = cidades;
     })
-        
-
   }
 
-  adicionar(nome:string){
-      console.log(nome);
+  public adicionar(nome:string){
+      this.cidadeService.adicionar({nome:nome})
+      .then(cidade =>{
+        //Esse alert é só por ter uma ideia
+        //Dentro desse método apenas devido a sincronicidade
+        //Se for fora do then vai executar de maneira assincrona
+          alert(`Cidade "${cidade.nome}" adicionada com sucesso para o código ${cidade.id} !`);
+          this.consultar();
+      });
   }
 
   excluir(id:number){
-      console.log(id);
+      this.cidadeService.excluir(id)
+        .then(()=>{
+          alert('Cidade excluida com sucesso !');
+          this.consultar();
+        })
   }
 
   atualizar(cidade:any){
-      console.log(JSON.stringify(cidade));
+      //console.log(JSON.stringify(cidade));
+      this.cidadeService.atualizar(cidade)
+        .then(() =>{
+          this.consultar();
+      })
+      .catch(erro =>{
+        alert(erro);
+      });
   }
-
 }
--- modulos/cursoModulo16/implementando-comunicacao-back-front/src/app/cidade.service.ts
+++ modulos/cursoModulo16/implementando-comunicacao-back-front/src/app/cidade.service.ts
@@ -7,11 +7,43 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CidadeService {
 
-  apiUrl:string = "http://localhost:3000/cidades";
+  private apiUrl:string = "http://localhost:3000/cidades";
 
   constructor(private http: HttpClient) { }
 
   consultar(): Promise<any> {
     return this.http.get(this.apiUrl).toPromise();
   }
+
+  adicionar(cidade:any): Promise<any> {
+    return this.http.post(this.apiUrl,cidade).toPromise();
+  }
+
+  excluir(id: number): Promise<void> {
+    // `` = Template Literals Apenas para não esquecer o nome
+    let caminhoRemocaoId:string = "/"+id;
+    let caminhoCompletoRemocaoId:string =this.apiUrl+caminhoRemocaoId; 
+
+    return this.http.delete(caminhoCompletoRemocaoId)
+      .toPromise()
+      .then(()=>null);
+
+  }
+
+  atualizar(cidade:any): Promise<any> {
+    let caminhoParcialAtualizacao:string = "/"+cidade.id;
+    /*let caminhoCompletoDeAtualizacao:string =this.apiUrl.substring(0,this.apiUrl.length-1).trim()
+                                             +caminhoParcialAtualizacao.trim(); */
+    let caminhoCompletoDeAtualizacao:string =this.apiUrl+caminhoParcialAtualizacao;
+
+    //Implementação dos Erros com o Retorno correto nos catchs
+    return this.http.put(caminhoCompletoDeAtualizacao,cidade)
+      .toPromise()
+      .catch(erro =>{
+        return Promise.reject(`Erro ao alterar cidade ${cidade.id}.`);
+      });
+  }
+
+
+
 }
